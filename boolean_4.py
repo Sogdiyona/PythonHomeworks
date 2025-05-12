50d2bb5100033ee501abaa951eb31d0986e86f16
Sogdiyona <sugdiyona0606@gmail.com>
2025-05-13 00:09:30 +0500

    Delete boolean_4.py

--- boolean_4.py
+++
@@ -1,7 +0,0 @@
-a=float(input("enter the 1st number: "))
-b=float(input("enter the 2nd number: "))
-c=float(input("enter the 3rd number: "))
-if a!=b and b!=c and a!=c and a!=b!=c:
-    print(" the numbers are different")
-else:
-    print("2 or all of them are equal") 
