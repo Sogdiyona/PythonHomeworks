ec5f5ea1abfe440b637d8814da84258e9c89bb00
Sogdiyona <sugdiyona0606@gmail.com>
2025-05-13 00:13:10 +0500

    Delete string_6.py

--- string_6.py
+++
@@ -1,6 +0,0 @@
-main_string=input("Enter the main string: ")
-substring= input("Enter the substring: ")
-if substring in main_string :
-    print(" The main string contains substring")
-else :
-    print(" The main string doesn't contain substring")
