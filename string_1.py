215f5d1ea3d47d2b2a8f37af8c6b067315da322a
Sogdiyona <sugdiyona0606@gmail.com>
2025-05-13 00:11:07 +0500

    Delete string_1.py

--- string_1.py
+++
@@ -1,4 +0,0 @@
-name=input("Enter your name,please: " )
-year=int(input("Enter your year of bitrh,please: "))
-print(f"{name}, you're {2025-year}")
-print("{name} you're {2025-(year)}". format(name=name, year=year))
